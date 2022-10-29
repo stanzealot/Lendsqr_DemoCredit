941c2a15a3721b00b85d4dbaa7f046ec2659cf2c
stanley omeje <stanley.omeje>
2022-10-29 22:37:59 +0100

    Setup: setup folder structure and configure express server

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import accountRouter from './routes/account'
+import usersRouter from './routes/users'
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/democredit', accountRouter);
+app.use('/users', usersRouter);
+
+export default app;
